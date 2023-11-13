dcc6d43ddb6215880745be88d59b6ad7b58e438b
Logan Bain <loganbain23@gmail.com>
2023-11-13 13:39:57 +0000

    L1: Local Definitions

--- mooc-l1-sandbox.hs
+++ mooc-l1-sandbox.hs
@@ -2,8 +2,10 @@
 -- where
 circleArea :: Double -> Double
 circleArea r = pi * rsquare
-    where pi = 3.1415926
+    where pi      = 3.1415926
           rsquare = r * r
+          -- local definitions can also be functions: 
+          -- rsquare x = x * x
 
 {- implementation with let...in
 circleArea r = let pi = 3.1415926
